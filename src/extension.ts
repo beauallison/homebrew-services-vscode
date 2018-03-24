085041efbacdfc178f6a5e8304351d19383ba699
Beau Allison <beauallison@users.noreply.github.com>
2018-03-24 09:46:52 +0000

    Add ability to view, start, and stop services

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,46 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+/* tslint:disable:no-duplicate-imports */
+
+import { TreeDataProvider, TreeItem } from 'vscode';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const brew = require('homebrew-services');
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "brew-services" is now active!');
+class BrewExplorer implements TreeDataProvider<any> {
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  private onDidChange: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
+  readonly onDidChangeTreeData = this.onDidChange.event;
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+  refresh() {
+    this.onDidChange.fire();
+  }
 
-    context.subscriptions.push(disposable);
+  public async getChildren() {
+    const { services } = await brew.list();
+    return [...services.entries()];
+  }
+
+  public getTreeItem(element: string[]): TreeItem {
+    return {
+      label: `${element[0]} status: ${element[1]}`,
+      contextValue: 'serviceItem',
+    };
+  }
+
+  public async execute(command: string, args: string[]) {
+    const { status } = await brew[command]({ service: args[0] });
+    this.refresh();
+    return vscode.window.showInformationMessage(`Service: ${args[0]} ${status}`);
+  }
 }
 
-// this method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+  const explorer = new BrewExplorer();
+  vscode.window.registerTreeDataProvider('brewExplorer', explorer);
+
+  vscode.commands.registerCommand('startService', args => explorer.execute('start', args));
+  vscode.commands.registerCommand('stopService', args => explorer.execute('stop', args));
+  vscode.commands.registerCommand('refreshButton', () => explorer.refresh());
+}
+
 export function deactivate() {
 }

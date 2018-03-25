b782d56b31c0a3c8615c544c877baa21baf420ff
Beau Allison <beauallison@users.noreply.github.com>
2018-03-25 15:55:17 +0100

    Add extension (#3)
    * Add coverage to gitignore
    
    * Modify structure for testing framework
    
    * Add homebrew-services and deps
    
    * Add menu icons
    
    * Add ability to view, start, and stop services
    
    * Alter sevice output status
    
    * Break out functions
    
    * Ingore build directory for jest tests
    
    * Exclude tests from build directory

---
+++ src/brewExplorer.ts
@@ -0,0 +1,35 @@
+/* tslint:disable:no-duplicate-imports */
+
+import * as vscode from 'vscode';
+import { TreeDataProvider, TreeItem } from 'vscode';
+import { upperFirst } from './helpers';
+
+const brew = require('homebrew-services');
+
+export default class BrewExplorer implements TreeDataProvider<any> {
+
+  private onDidChange: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
+  readonly onDidChangeTreeData = this.onDidChange.event;
+
+  refresh() {
+    this.onDidChange.fire();
+  }
+
+  public async getChildren() {
+    const { services } = await brew.list();
+    return [...services.entries()];
+  }
+
+  public getTreeItem(element: string[]): TreeItem {
+    return {
+      label: `${upperFirst(element[0])}: ${upperFirst(element[1])}`,
+      contextValue: 'serviceItem',
+    };
+  }
+
+  public async execute(command: string, args: string[]) {
+    const { status } = await brew[command]({ service: args[0] });
+    this.refresh();
+    return vscode.window.showInformationMessage(`Service: ${upperFirst(args[0])} ${status}`);
+  }
+}

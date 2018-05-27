7a6a1a0788d2097c67f939ca395fc8feeff08ec7
Beau Allison <beauallison@users.noreply.github.com>
2018-05-27 17:32:09 +0100

    Create statusConversion helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,18 @@
 export function upperFirst(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+interface status {
+  restart: string;
+  start: string;
+  stop: string;
+  [key: string]: string;
+}
+
+const statusConversion: status = {
+  restart: 'Restarting',
+  start: 'Starting',
+  stop: 'Stopping',
+};
+
+export { statusConversion };
